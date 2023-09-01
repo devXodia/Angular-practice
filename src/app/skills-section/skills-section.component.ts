80c8ab8041ba708c5bdfce1ef826e59d6e50dabf
Alen Alduk <youstylez@gmail.com>
2023-09-01 16:15:22 +0200

    added skills page

--- src/app/skills-section/skills-section.component.ts
+++ src/app/skills-section/skills-section.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills-section.component.scss']
 })
 export class SkillsSectionComponent {
+  readonly baseUrl = '../../assets/img/skills';
 
+ skillList = [
+  {
+    nameOfSkill: 'Java script',
+    logo: `${this.baseUrl}/javascript.png`
+  },
+  {
+    nameOfSkill: 'Angular',
+    logo: `${this.baseUrl}/angular.png`
+  },
+  {
+    nameOfSkill: 'HTML / CSS',
+    logo: `${this.baseUrl}/html.png`
+  },
+  {
+    nameOfSkill: 'SCRUM',
+    logo: `${this.baseUrl}/scrum.png`
+  },
+  {
+    nameOfSkill: 'Git',
+    logo: `${this.baseUrl}/git.png`
+  },
+  {
+    nameOfSkill: 'Design Thinking',
+    logo: `${this.baseUrl}/think.png`
+  },
+  {
+    nameOfSkill: 'Rest API',
+    logo: `${this.baseUrl}/api.png`
+  },
+  {
+    nameOfSkill: 'Test automation',
+    logo: `${this.baseUrl}/test.png`
+  },
+  {
+    nameOfSkill: 'Databases',
+    logo: `${this.baseUrl}/db.png`
+  }
+ ]
 }
